9ed9517bc3c0514e8955232d5e180ffcef297bc5
kettlemorainerc <kettlemorainerc@gmail.com>
2026-07-13 19:27:28 -0500

    Added comments into ChangeCentricity
    Added comments explaining the functionality of ChangeCentricity.java

--- src/main/java/frc/robot/subsystems/ChangeCentricity.java
+++ src/main/java/frc/robot/subsystems/ChangeCentricity.java
@@ -20,6 +20,10 @@ public class ChangeCentricity implements Subsystem{
     
     private RobotHardware hardware = RobotHardware.getInstance();
 
+    /* This system is responsible for allowing the driver to change between robot centric and field centric
+     * This first block of code creates two diffenet drive systems of robot and field centric respectively
+     * This then allows for code to change what the primary drive type is between the two centricities
+     */
     public static double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     public static double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
@@ -33,7 +37,7 @@ public class ChangeCentricity implements Subsystem{
     public static SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     public static SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
-
+    // Gets the drive xbox controler to allow for driving 
     private CommandSwerveDrivetrain drivetrain = RobotHardware.getInstance().drivetrain;
     private CommandXboxController driveNewJoystick = null;
 
@@ -41,6 +45,9 @@ public class ChangeCentricity implements Subsystem{
         this.driveNewJoystick = driveNewJoystick;
     }
 
+    /* This sets the robot centricity to field centric
+     * It flips what each control is when on blue alliance due to how the orientation is when on the field
+     */
     public void setFieldCentric(){
         drivetrain.setControl(
             fieldDrive.withVelocityX(driveNewJoystick.getLeftY() * MaxSpeed * hardware.speedLimiterDrive) // Drive forward with negative Y (forward)
@@ -64,6 +71,7 @@ public class ChangeCentricity implements Subsystem{
         
     }
 
+    // Sets robot centricity to robot centric
     public void setRobotCentric(){
         drivetrain.setControl(
             robotDrive.withVelocityX(-driveNewJoystick.getLeftY() * MaxSpeed * 0.5) // Drive forward with negative Y (forward)
@@ -72,6 +80,7 @@ public class ChangeCentricity implements Subsystem{
             );
     }
 
+    // Sets robot centricity on the robot to backwards so that the back is now the front
     public void setBackwards(){
         drivetrain.setControl(
             robotDrive.withVelocityX(driveNewJoystick.getLeftY() * MaxSpeed * 0.5) // Drive forward with negative Y (forward)
